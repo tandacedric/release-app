284e67ebec0daf1b3599f6b7ebf3853d9d8bf669
cedric tanda <tandacedric@gmail.com>
2024-02-19 13:16:20 +0100

    Updates

---
+++ precache-manifest.c095b4f68a9683d7ab62311df55c6391.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab599a47ed14a15e177bf96a573740fa",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4a6fb73a59417f6091fb",
+    "url": "./static/css/main.b9f1cc03.chunk.css"
+  },
+  {
+    "revision": "21d980e1025b684b4f72",
+    "url": "./static/js/2.55375e64.chunk.js"
+  },
+  {
+    "revision": "4a6fb73a59417f6091fb",
+    "url": "./static/js/main.d65ada13.chunk.js"
+  },
+  {
+    "revision": "474d70510a2c5ad46798",
+    "url": "./static/js/runtime-main.b92a5e9f.js"
+  },
+  {
+    "revision": "a1c7e61496e9323c39256c372c1e4cff",
+    "url": "./static/media/logo.a1c7e614.png"
+  },
+  {
+    "revision": "97f683728e77b1ad8dd97023e47e1ecc",
+    "url": "./static/media/money.97f68372.png"
+  },
+  {
+    "revision": "eb77bfb55efc0021e68a6186ce1cadc1",
+    "url": "./static/media/p1.eb77bfb5.jpg"
+  },
+  {
+    "revision": "d13486a103ab741205326db427566332",
+    "url": "./static/media/p2.d13486a1.jpg"
+  },
+  {
+    "revision": "2d13453bfed9b8622b5e2abd7399d7c9",
+    "url": "./static/media/p3.2d13453b.jpg"
+  },
+  {
+    "revision": "b33b9181020763573debbb34c3920778",
+    "url": "./static/media/p4.b33b9181.jpg"
+  },
+  {
+    "revision": "b4ea08bb3b40e600dbe457608289ccb3",
+    "url": "./static/media/p5.b4ea08bb.jpg"
+  },
+  {
+    "revision": "57b8821639388c70be74a4d8a3a54ff7",
+    "url": "./static/media/p6.57b88216.jpg"
+  },
+  {
+    "revision": "d05a129e4b4ca30d671284b69384ffcc",
+    "url": "./static/media/salaire.d05a129e.png"
+  },
+  {
+    "revision": "4164b7fa32d18a065900748edc6c4b09",
+    "url": "./static/media/shape2.4164b7fa.png"
+  },
+  {
+    "revision": "ca16b4760522825ea825f5a3b09e5b51",
+    "url": "./static/media/sheet.ca16b476.png"
+  }
+]);
